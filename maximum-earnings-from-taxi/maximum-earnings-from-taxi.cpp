f19e3299eaadb3b3dee18028e40d6f9165e2279a
Pratyush Kumar <66811279+kpratyush12345@users.noreply.github.com>
2021-09-19 10:44:53 +0530

    Time: 472 ms (100.00%), Space: 106.9 MB (36.36%) - LeetHub

--- maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
+++ maximum-earnings-from-taxi/maximum-earnings-from-taxi.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
-    static bool comp(vector<int> &a,vector<int> &b){
-        return(a[1] < b[1]);
-    }
-	
-    int check(vector<vector<int>>& r, int i){
-        int lo = 0, hi = i- 1;
-        while (lo <= hi){
-            int mid = (lo + hi) / 2;
-            if (r[mid][1] <= r[i][0]){
-                if (r[mid + 1][1] <= r[i][0]) lo = mid + 1;
-                else return mid;
-            }
-            else hi = mid - 1;
+    long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
+        sort(rides.begin(), rides.end());
+        vector<long long> dp(n+1);
+        int j = 0;
+        for(int i = 1; i <= n; ++i) {
+            dp[i] = max(dp[i], dp[i - 1]);
+            while (j < rides.size() && rides[j][0] == i)
+                dp[rides[j++][1]] = max(dp[rides[j][1]], dp[i] + rides[j][1] - rides[j][0] + rides[j][2]);
         }
-        return -1;
-    }
-    
-    long long maxTaxiEarnings(int n, vector<vector<int>>& r) {
-        sort(r.begin(),r.end(),comp);
-        vector<long long> res(r.size());
-        res[0] = r[0][1] - r[0][0] + r[0][2];
-        for(int i = 1;i<r.size();i++){
-            long long in = r[i][1] - r[i][0] + r[i][2];
-            int l = check(r, i);
-            if(l != -1) in += res[l];
-            res[i] = max(in, res[i-1]);
-        }
-        return(res[r.size() - 1]);
+        return dp[n];
     }
 };
